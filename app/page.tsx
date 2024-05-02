983aa8dbae6f0f38ccf92cd89dc5ef9e4cf45d0f
Daiane Bolzan <dainha85@gmail.com>
2024-05-02 10:25:31 -0300

    fix: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Search />
+      <div className="px-5 pt-6"></div>
+    </>
+  );
 }
