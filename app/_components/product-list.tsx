a96139ba3fbf1859a171b6923a72f97b2993eb8f
Daiane Bolzan <dainha85@gmail.com>
2024-05-03 21:26:27 -0300

    feat: update products

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,14 +1,21 @@
 import { Prisma } from "@prisma/client";
 import ProductItem from "./product-item";
+
 interface ProductListProps {
   products: Prisma.ProductGetPayload<{
-    include: { restaurant: { select: { name: true } } };
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
   }>[];
 }
 
-const ProductList = async ({ products }: ProductListProps) => {
+const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex overflow-x-scroll [&::-webkit-scrollbar]:hidden gap-4">
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
